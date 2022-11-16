20e04a792ae060ebafa50a8eff330e3ca2aa46f7
Yura Korokvo <ykorovko@gmail.com>
2022-11-16 15:45:00 +0200

    help

--- functions/bot.js
+++ functions/bot.js
@@ -3,13 +3,30 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start(ctx => {
   console.log("Received /start command")
-  
+
   try {
-    return ctx.reply("Hi")
+    return ctx.reply(`Hi ${ctx.from.first_name}!`)
   } catch (e) {
     console.error("error in start action:", e)
     return ctx.reply("Error occured")
   }
+})
+
+bot.help((ctx) => {
+  ctx.reply('Send /start to receive a greeting')
+  ctx.reply('Send /authcode to get an auth code')
+  ctx.reply('Send /quit to stop the bot')
+});
+
+const authCode = 'test'
+
+bot.command('authcode', (ctx) => ctx.reply(authCode));
+
+bot.command('quit', (ctx) => {
+  // Explicit usage
+  ctx.telegram.leaveChat(ctx.message.chat.id)
+// Context shortcut
+  ctx.leaveChat()
 })
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
